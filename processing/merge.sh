259d143c5decd738302ac40d7ab4b6edaeb88181
Shawn Allen <shawn.allen@gsa.gov>
2015-03-26 14:13:55 -0700

    fix .shp paths

--- processing/merge.sh
+++ processing/merge.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 SRID="EPSG:900913"
 echo "importing data..."
-time shp2pgsql -d -I -s $SRID tilemill/fedland/data/fedland.shp fedland | psql eiti-maps
-time shp2pgsql -d -I -s $SRID tilemill/tribal-areas/data/tribal-lands.shp tribal | psql eiti-maps
+time shp2pgsql -d -I -s $SRID ../tilemill/fedland/data/fedland.shp fedland | psql eiti-maps
+time shp2pgsql -d -I -s $SRID ../tilemill/tribal-areas/data/tribal-lands.shp tribal | psql eiti-maps
 echo "collecting geometries..."
 time cat merge-collect.sql | psql eiti-maps
 echo "union-ing..."
