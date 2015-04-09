8741fc74608b97925aa14217b35c05f50daeccf3
Shawn Allen <shawn.allen@gsa.gov>
2015-04-09 15:46:48 -0700

    use utils, etc.

--- counties/data/test-county-revenues.js
+++ counties/data/test-county-revenues.js
@@ -1,6 +1,9 @@
-var fs = require('fs');
 var tito = require('tito');
 var async = require('async');
+var util = require('./util');
+
+var read = util.readData;
+var map = util.map;
 
 async.parallel({
   revenues: readRevenues,
@@ -9,13 +12,15 @@ async.parallel({
 }, function(error, results) {
   if (error) return console.log('Error:', error);
 
-  var countiesByFIPS = map(results.counties, 'FIPS');
-  var statesByFIPS = map(results.states, 'FIPS');
-  var statesByAbbr = map(results.states, 'abbr');
+  results.revenues.forEach(function(d, i) {
+    d.$line = i;
+  });
+
+  var statesByAbbr = map(results.states, 'abbr', true);
   var revenuesByState = map(results.revenues, 'St');
 
   for (var state in revenuesByState) {
-    var goodFIPS = statesByAbbr[state][0].FIPS;
+    var goodFIPS = statesByAbbr[state].FIPS;
     var row = revenuesByState[state][0];
     var checkFIPS = row['County Code'].substr(0, 2);
     if (checkFIPS != goodFIPS) {
@@ -24,92 +29,34 @@ async.parallel({
         // 'mismatch:',
         checkFIPS, '(' + row.St + ') should be',
         goodFIPS,
-        'on lines:', sequences(lines, 2)
+        'on lines:', util.sequences(lines, 2)
       );
     }
   }
 });
 
 function readRevenues(done) {
-  return read('county-revenues.tsv',
+  return read(
+    'input/county-revenues.tsv',
     tito.formats.createReadStream('tsv'),
-    done);
+    done
+  );
 }
 
 function readCounties(done) {
-  return read('geo/us-counties.json',
+  return read(
+    'geo/us-counties.json',
     tito.formats.createReadStream('json', {
       path: 'objects.counties.geometries.*.properties'
     }),
-    done);
+    done
+  );
 }
 
 function readStates(done) {
-  return read('states.csv',
+  return read(
+    'input/states.csv',
     tito.formats.createReadStream('csv'),
-    done);
-}
-
-function read(filename, parse, done) {
-  var data = [];
-  fs.createReadStream(filename)
-    .pipe(parse)
-    .on('data', function(d) {
-      d.$line = data.length;
-      data.push(d);
-    })
-    .on('error', done)
-    .on('end', function() {
-      done(null, data);
-    });
-}
-
-function map(list, key) {
-  key = getter(key);
-  var map = {};
-  list.forEach(function(d) {
-    var k = key(d);
-    if (map.hasOwnProperty(k)) {
-      map[k].push(d);
-    } else {
-      map[k] = [d];
-    }
-  });
-  return map;
-}
-
-function getter(key) {
-  if (!key) return identity;
-  else if (typeof key === 'function') return key;
-  return function(d) { return d[key]; };
-}
-
-function identity(d) {
-  return d;
-}
-
-function sequences(lines, offset) {
-  offset = offset || 0;
-  var len = lines.length;
-  if (len < 2) return lines.join(', ');
-  var seq = [];
-  var l0 = lines[0];
-  var last = l0;
-  for (var i = 1; i < len; i++) {
-    if (lines[i] === last + 1) {
-      last = lines[i];
-      continue;
-    }
-    last = lines[i - 1];
-    if (l0 === last) {
-      seq.push(last + offset);
-    } else {
-      seq.push([l0 + offset, last + offset].join('-'));
-    }
-    l0 = last = lines[i];
-  }
-  if (l0 === last) {
-    seq.push(last + offset);
-  }
-  return seq.join(', ');
+    done
+  );
 }
